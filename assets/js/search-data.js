f7da46e7d60cbfd1b09c4c42c4b79f34b90189dd
BruDiGe <97832125+BruDiGe@users.noreply.github.com>
2026-01-04 00:44:23 +0000

    Deploying to gh-pages from @ BruDiGe/brudige.github.io@e7d018bbef54ddd75099d188707c56b4a89b0cf1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,12 +408,7 @@ ninja.data = [{
             },},{id: "news-invited-to-serve-on-the-external-advisory-board-of-the-fwf-doc-funds-doctoral-network-biotechpredict-invitation-letter-pdf",
           title: 'Invited to serve on the External Advisory Board of the FWF doc.funds doctoral...',
           description: "",
-          section: "News",},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
+          section: "News",},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
